e333e96056891069ced2cbdc8c3b08bf692a4eaa
Marrlika <Marrlika@outlook.com>
2020-03-19 20:00:43 +0300

    feat: complete vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,65 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    notReverseFlag = true;
+
+    constructor (value) {
+        this.notReverseFlag = value;
+    }
+    
+    encrypt(message, key) {
+        let j = 0;
+        key = key.toUpperCase();
+        if (this.notReverseFlag == false) {
+            message = message.split('').reverse().join('');
+        }
+        let newMessage = message.toUpperCase().split(" ").map(element => {
+            let newStr = "";
+            let numLetter, newNumLetter;
+            for (let i=0; i < element.length ; i++){
+                numLetter = element.codePointAt(i);
+                if (numLetter<65||numLetter>90) {
+                    newNumLetter = numLetter;
+                } else {
+                    if  (j>=key.length) {j = 0;}
+                    newNumLetter = numLetter + key.codePointAt(j) - 65;
+                    j++;
+                    if (newNumLetter>90) {
+                        newNumLetter -= 26;
+                    }
+                }
+                newStr += String.fromCodePoint(newNumLetter);
+            }
+            return newStr;
+        });
+        
+        return newMessage.join(" ");
+        
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(message, key) {
+        let j = 0;
+        key = key.toUpperCase();
+        if (this.notReverseFlag == false) {
+            message = message.split('').reverse().join('');}
+        let newMessage = message.toUpperCase().split(" ").map(element => {
+            let newStr = "";
+            let numLetter, newNumLetter;
+            for (let i=0; i < element.length ; i++){
+                numLetter = element.codePointAt(i);
+                if (numLetter<65||numLetter>90) {
+                    newNumLetter = numLetter;
+                } else {
+                    if  (j>=key.length) {j = 0;}
+                    newNumLetter = numLetter - key.codePointAt(j)+65;
+                    j++;
+                    if (newNumLetter<65) {
+                        newNumLetter += 26;
+                    }
+                }
+                newStr += String.fromCodePoint(newNumLetter);
+            }
+            return newStr;
+        });
+        return newMessage.join(" ");
     }
 }
 
