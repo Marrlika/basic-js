38739b32531e7b455bda83924d28203b47956411
Marrlika <Marrlika@outlook.com>
2020-03-19 22:45:30 +0300

    feat: complete carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,12 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15; 
+const HALF_LIFE_PERIOD = 5730;
+const REACTION_RATE = 0.693/HALF_LIFE_PERIOD;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample( sampleActivity ) {
+  if(typeof sampleActivity != "string"||!(parseFloat(sampleActivity))) return false;
+  /*console.log(+sampleActivity);*/
+
+  let dateSample = Math.ceil(Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity))/REACTION_RATE);
+  return (!dateSample||dateSample===Infinity||dateSample<0)?false:dateSample;
 };
+
