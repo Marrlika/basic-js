57f8335eac03b199a84172fbb6dc9993ceffe548
Marrlika <Marrlika@outlook.com>
2020-03-22 20:24:29 +0300

    feat: complete what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,6 @@
 module.exports = function getSeason( date ) {
   if (!date) return "Unable to determine the time of year!";
+  if (date.hasOwnProperty('getMonth')) throw Error;
   if(date.getMonth()>=2&&date.getMonth()<=4) {return "spring";}
   if(date.getMonth()>=5&&date.getMonth()<=7) {return "summer";}
   if(date.getMonth()>=8&&date.getMonth()<=10) {return "fall";}
